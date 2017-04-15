255fda5d6d4b4267f5731338ad5e4918277bf838
MrModder1234 <treehouseacademy4@gmail.com>
2017-04-14 21:55:34 -0400

    Update ModEntities.java
    Changed the hexadecimals to be accurate to the species.

--- src/main/java/com/africacraft/entity/ModEntities.java
+++ src/main/java/com/africacraft/entity/ModEntities.java
@@ -24,28 +24,27 @@ import net.minecraft.init.Biomes;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class ModEntities {
-
-
+	
 public static void registerEntity(){
 	        // Every entity in our mod has an ID (local to this mod)
 	        int id = 1;
-	        EntityRegistry.registerModEntity(EntitySpottedHyena.class, "Spotted Hyena", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityLion.class, "African Lion Male", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityAfricaCraftElephant.class, "African Elephant", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityGiraffe.class, "Masai Giraffe", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityGiraffe2.class, "Reticulated Giraffe", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityGiraffe3.class, "Rothschild Giraffe", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityAfricanWildDog.class, "African Wild Dog", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityWildebeest.class, "Wildebeest", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityHippo.class, "Hippopotamus", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityLioness.class, "African Lion Female", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityGemsbok.class, "Gemsbok", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityGazelle.class, "Thomson's Gazelle", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityPlainsZebra.class, "Savanna Zebra", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityAfricanLeopard.class, "African Leopard", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityBlackLeopard.class, "Black Leopard", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityEland.class, "Common Eland", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
-	        EntityRegistry.registerModEntity(EntityRatel.class, "Honey Badger", id++, ACMain.instance, 64, 3, true, 0x996600, 0x00ff00);
+	        EntityRegistry.registerModEntity(EntitySpottedHyena.class, "Spotted Hyena", id++, ACMain.instance, 64, 3, true, 0xffcc66, 0x7e4e41);
+	        EntityRegistry.registerModEntity(EntityLion.class, "African Lion Male", id++, ACMain.instance, 64, 3, true, 0xe3bd60, 0x63482e);
+		EntityRegistry.registerModEntity(EntityLioness.class, "African Lion Female", id++, ACMain.instance, 64, 3, true, 0xe3bd60, 0x63482e);
+	        EntityRegistry.registerModEntity(EntityAfricaCraftElephant.class, "African Elephant", id++, ACMain.instance, 64, 3, true, 0x606060, 0x363535);
+	        EntityRegistry.registerModEntity(EntityGiraffe.class, "Masai Giraffe", id++, ACMain.instance, 64, 3, true, 0xffb870, 0x662d10);
+	        EntityRegistry.registerModEntity(EntityGiraffe2.class, "Reticulated Giraffe", id++, ACMain.instance, 64, 3, true, 0x662d10, 0xffb870);
+	        EntityRegistry.registerModEntity(EntityGiraffe3.class, "Rothschild Giraffe", id++, ACMain.instance, 64, 3, true, 0xffb870, 0xcc5a20);
+	        EntityRegistry.registerModEntity(EntityAfricanWildDog.class, "African Wild Dog", id++, ACMain.instance, 64, 3, true, 0xff7129, 0x2f2e2e);
+	        EntityRegistry.registerModEntity(EntityWildebeest.class, "Wildebeest", id++, ACMain.instance, 64, 3, true, 0xbf9367, 0x383533);
+	        EntityRegistry.registerModEntity(EntityHippo.class, "Hippopotamus", id++, ACMain.instance, 64, 3, true, 0x724d6a, 0xeea3df);
+	        EntityRegistry.registerModEntity(EntityGemsbok.class, "Gemsbok", id++, ACMain.instance, 64, 3, true, 0xdfc091, 0x363535);
+	        EntityRegistry.registerModEntity(EntityGazelle.class, "Thomson's Gazelle", id++, ACMain.instance, 64, 3, true, 0xdda95a, 0xececec);
+	        EntityRegistry.registerModEntity(EntityPlainsZebra.class, "Savanna Zebra", id++, ACMain.instance, 64, 3, true, 0xfbfbfb, 0x313131);
+	        EntityRegistry.registerModEntity(EntityAfricanLeopard.class, "African Leopard", id++, ACMain.instance, 64, 3, true, 0xd4b793, 0x3e3e3e);
+	        EntityRegistry.registerModEntity(EntityBlackLeopard.class, "Black Leopard", id++, ACMain.instance, 64, 3, true, 0x3e3e3e, 0xc1bbb5);
+	        EntityRegistry.registerModEntity(EntityEland.class, "Common Eland", id++, ACMain.instance, 64, 3, true, 0xf4cb97, 0xd4d4d4);
+	        EntityRegistry.registerModEntity(EntityRatel.class, "Honey Badger", id++, ACMain.instance, 64, 3, true, 0x63431d, 0xdfb37d);
 
 	addNaturalSpawns();
 }
